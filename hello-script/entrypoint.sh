6e1fe8a74e8bb8d6d5695fd78666e0b54f38afca
John Dietz <john@kubefirst.com>
2024-04-10 01:50:29 -0400

    env var for command string and update to build output

--- hello-script/entrypoint.sh
+++ hello-script/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Check if exactly one argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <command>"
-    exit 1
-fi
-
 # Store the command string from the argument
-cmd_string="$1"
+cmd_string="$COMMAND_STRING"
 
 # Print the command to be executed
 echo "Executing command: $cmd_string"
